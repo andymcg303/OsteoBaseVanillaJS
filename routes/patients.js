53dc9039f77941f6d1112d70cb7c0a942bd88116
andymcg303 <mcgow_a@hotmail.com>
2019-11-15 22:36:01 +0000

    adding bs4

--- routes/patients.js
+++ routes/patients.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
         if (err){
             console.log(err);
         } else {
-            res.render("./patients/index", {patients: allPatients});
+            res.render("./patients/index", {patients: allPatients, moment: moment});
         }
     });
 });
