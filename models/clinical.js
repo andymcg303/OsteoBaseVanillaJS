db0953338457cdbcfa95bc7236a0cd9338edde0b
andymcg303 <mcgow_a@hotmail.com>
2019-12-09 08:20:47 +0000

    superficially finished show/edit clinicals

--- models/clinical.js
+++ models/clinical.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const clinicalSchema = new mongoose.Schema({
     date_created: {type: Date, default: Date.now},
     OE: String,
-    diag_bool: Boolean,
     diag: String,
     TTT: String
 });
