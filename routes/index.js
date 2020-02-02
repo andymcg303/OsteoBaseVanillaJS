8472d67fe795f8c009e4423b421d0906359e8673
andymcg303 <mcgow_a@hotmail.com>
2020-02-02 20:44:07 +0000

    working on login funtionality

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,39 @@
 const express   = require("express"),
       router    = express.Router();       
 
-// Root Landing Page Route   
+// Root Landing Page ie login   
 router.get("/", (req, res) => res.render("login"));
 
 // Show Login Form
-router.get("/signup", function(req, res){
-	res.render("signup");
-})
+router.get("/login", (req, res) => res.render("login"));
+
+// Show Sign Up Form
+router.get("/signup", (req, res) => res.render("signup"));
+
+//Handle Signup Logic
+router.post("/signup", (req, res) => {
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, (err) => {
+		if (err){
+            // req.flash("error", err.message);
+			return res.redirect("/signup")
+		}
+		passport.authenticate("local")(req, res, () => {
+            // req.flash("success", "Welcome to OsteoBase" + user.username);
+			res.redirect("/patients");
+		});
+	});
+});
+
+// Handle Login Logic
+router.post("/login", function(req, res, next){
+	passport.authenticate("local", 
+	{
+		successRedirect: "/patients",
+		failureRedirect: "/login"
+		// failureFlash: true,
+		// successFlash: "Welcome to YelpCamp, " + req.body.username + "!"
+	})(req, res); 
+});
 
 module.exports = router;
