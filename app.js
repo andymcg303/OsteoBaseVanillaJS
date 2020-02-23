7d99f8c3b448538010719c7e28bb1388e1433c46
andymcg303 <mcgow_a@hotmail.com>
2020-02-23 21:43:37 +0000

    taking a break from this while doing node course

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express           = require("express"),
-      app               = express(),
-      bodyParser        = require("body-parser"),
-      mongoose          = require("mongoose"),
-      methodOverride	= require("method-override")
-      passport          = require("passport"),
-      LocalStrategy	    = require("passport-local")
-      User              = require("./models/user");
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 // configure route requires
-const indexRoutes = require("./routes/index"),
-      patientsRoutes = require("./routes/patients"),
-      interviewsRoutes = require("./routes/interviews"),
-      medhistsRoutes = require("./routes/medhists"),
-      clinicalsRoutes = require("./routes/clinicals");
+const indexRoutes = require('./routes/index');
+const patientsRoutes = require('./routes/patients');
+const interviewsRoutes = require('./routes/interviews');
+const medhistsRoutes = require('./routes/medhists');
+const clinicalsRoutes = require('./routes/clinicals');
 
 mongoose.connect(process.env.DATABASEURL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 }).then(() => {
-    console.log('Connected to DB!');
-}).catch(err => {
-    console.log('ERROR:', err.message);
+  console.log('Connected to DB!');
+}).catch((err) => {
+  console.log('ERROR:', err.message);
 });
 
 mongoose.set('useFindAndModify', false);
 
 // app configuration
-app.set("view engine", "ejs");   
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(methodOverride("_method"));
-app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(`${__dirname}/public`));
 
-//PASSPORT CONFIGURATION
-app.use(require("express-session")({
-	secret: "I Love Rosie",
-	resave: false,
-	saveUninitialized: false
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+  secret: 'I Love Rosie',
+  resave: false,
+  saveUninitialized: false,
 }));
 
 app.use(passport.initialize());
@@ -47,19 +48,19 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next){
-	res.locals.currentUser = req.user;
-	// res.locals.error = req.flash("error");
-	// res.locals.success = req.flash("success");
-	next();
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  // res.locals.error = req.flash("error");
+  // res.locals.success = req.flash("success");
+  next();
 });
 
 
 // Has to be done after app.use(bodyParser)
 app.use(indexRoutes);
-app.use("/patients", patientsRoutes);
-app.use("/patients/:id/interviews", interviewsRoutes);
-app.use("/patients/:id/medhists", medhistsRoutes);
-app.use("/patients/:id/clinicals", clinicalsRoutes);
+app.use('/patients', patientsRoutes);
+app.use('/patients/:id/interviews', interviewsRoutes);
+app.use('/patients/:id/medhists', medhistsRoutes);
+app.use('/patients/:id/clinicals', clinicalsRoutes);
 
-app.listen(3000, () => console.log("OsteoBase Server Started"));
+app.listen(3000, () => console.log('OsteoBase Server Started'));
--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,48 @@
-const express   = require("express"),
-      router    = express.Router();       
+const express = require('express');
+const passport = require('passport');
+const User = require('../models/user');
 
-// Root Landing Page ie login   
-router.get("/", (req, res) => res.render("login"));
+const router = express.Router();
+
+// Root Landing Page ie login
+router.get('/', (req, res) => res.render('login'));
 
 // Show Login Form
-router.get("/login", (req, res) => res.render("login"));
+router.get('/login', (req, res) => res.render('login'));
 
 // Show Sign Up Form
-router.get("/signup", (req, res) => res.render("signup"));
+router.get('/signup', (req, res) => res.render('signup'));
 
-//Handle Signup Logic
-router.post("/signup", (req, res) => {
-	var newUser = new User({username: req.body.username});
-	User.register(newUser, req.body.password, (err) => {
-		if (err){
-            // req.flash("error", err.message);
-			return res.redirect("/signup")
-		}
-		passport.authenticate("local")(req, res, () => {
-            // req.flash("success", "Welcome to OsteoBase" + user.username);
-			res.redirect("/patients");
-		});
-	});
+// Handle Signup Logic
+router.post('/signup', (req, res) => {
+  const newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, (err) => {
+    if (err) {
+      // req.flash("error", err.message);
+      return res.redirect('/signup');
+    }
+    passport.authenticate('local')(req, res, () => {
+      // req.flash("success", "Welcome to OsteoBase" + user.username);
+      res.redirect('/patients');
+    });
+  });
 });
 
 // Handle Login Logic
-router.post("/login", function(req, res, next){
-	passport.authenticate("local", 
-	{
-		successRedirect: "/patients",
-		failureRedirect: "/login"
-		// failureFlash: true,
-		// successFlash: "Welcome to YelpCamp, " + req.body.username + "!"
-	})(req, res); 
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local',
+    {
+      successRedirect: '/patients',
+      failureRedirect: '/login',
+      // failureFlash: true,
+      // successFlash: "Welcome to YelpCamp, " + req.body.username + "!"
+    })(req, res);
 });
 
 // Logout
-router.get("/logout", function(req, res){
-	req.logout();
-	res.redirect("/login");
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
 });
 
 module.exports = router;
