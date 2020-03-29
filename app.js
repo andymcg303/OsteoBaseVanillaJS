95c61096410d88bf7126fecd31069989c21e591d
andymcg303 <mcgow_a@hotmail.com>
2020-03-29 22:02:53 +0100

    const createError defined in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 
+const createError = require('http-errors');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
