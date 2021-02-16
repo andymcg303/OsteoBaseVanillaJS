70863b468a3775e16cbc762bf7e0a6c6d5610b96
Andy McG <mcgow_a@hotmail.com>
2021-02-16 18:37:14 +0000

    working on removing JQuery from patient functionality

--- public/js/patient.js
+++ public/js/patient.js
@@ -1,63 +1,95 @@
-$(function(){
+$.fn.dataTable.moment( 'DD/MM/YYYY' );
 
-    $.fn.dataTable.moment( 'DD/MM/YYYY' );
+// Patient Index DataTable initialisation
+const $patientTable = $('#patient-table').DataTable( {
+    "columnDefs":  
+        [{
+            "targets": [ 0 ],
+            "visible": false
+        }],
+        "order": [ 0, 'desc' ],
+        "lengthMenu": [[10, 20, -1], [10, 20, "All"]]
+    } );
 
-    // Patient Index DataTable initialisation
-    const $patientTable = $('#patient-table').DataTable( {
-        "columnDefs":  
-            [{
-                "targets": [ 0 ],
-                "visible": false
-            }],
-            "order": [ 0, 'desc' ],
-            "lengthMenu": [[10, 20, -1], [10, 20, "All"]]
-        } );
+// Prevents flicker on loading
+$('#patient-table').show();    
 
-    // Prevents flicker on loading
-    $('#patient-table').show();    
+// open patient view
+$('#patient-table tbody').on('click', 'tr', function () {
+    const data = $patientTable.row( this ).data();
+    const urlParams = new URLSearchParams(window.location.search);
+    const currentView = urlParams.get('currentView');
+    window.location.assign(`patients/${data[0]}?currentView=${currentView}`);
+});
 
-    // open patient view
-    $('#patient-table tbody').on('click', 'tr', function () {
-        const data = $patientTable.row( this ).data();
-        const urlParams = new URLSearchParams(window.location.search);
-        const currentView = urlParams.get('currentView');
-        window.location.assign(`patients/${data[0]}?currentView=${currentView}`);
-    });
+// // PATIENT INDEX
+const newPatientButton = document.querySelector('#new-patient-button');
+const newPatientForm = document.querySelector('#new-patient-form');
+const formControls = document.querySelectorAll('.form-control');
+// Show New Patient form
+newPatientButton.addEventListener('click', function(){
+    newPatientForm.style.display = 'block';
+    this.style.display = 'none';
+});
 
-    // // PATIENT INDEX
-    const newPatientButton = document.querySelector('#new-patient-button');
-    const newPatientForm = document.querySelector('#new-patient-form');
+// Hide new patient form on cancel
+document.querySelector('#cancel-new-patient').addEventListener('click', () => {
+    newPatientForm.style.display = 'none';
+    formControls.forEach(el => el.value = '');
+    newPatientButton.style.display = 'block';
+});
 
-    // Show New Patient form
-    newPatientButton.addEventListener('click', function(){
-        newPatientForm.style.display = 'block';
-        this.style.display = 'none';
-    });
+var serializeForm = function (form) {
+    var obj = {};
+    var formData = new FormData(form);
+    for (var key of formData.keys()) {
+        obj[key] = formData.get(key);
+    }
+    return obj;
+};
 
-    // Hide new patient form
-    document.querySelector('#cancel-new-patient').addEventListener('click', () => {
+// // Post new patient
+// newPatientForm.addEventListener('submit', e => {    
+//     e.preventDefault();
+//     const newPatientData = JSON.stringify(serializeForm(e.target));
+//     $.post('/patients', newPatientData, function(data){
+//         $patientTable.row.add( [ 
+//             `${data._id}`,
+//             `${data.surname}`,
+//             `${data.firstname}`,
+//             `${moment(data.dob).format('DD/MM/YYYY')}`,
+//             `${data.phonenumber}`] )
+//         .draw();
+//         newPatientForm.find('.form-control').val('');
+//         newPatientButton.prop('disabled',false);
+//         newPatientForm.toggle();
+//         newPatientButton.toggle();    
+//     });
+
+// });
+
+// // No JQuery Post new patient
+newPatientForm.addEventListener('submit', e => {    
+    e.preventDefault();
+    const newPatientData =JSON.stringify(serializeForm(e.target));
+    fetch('/patients', {
+        method: 'POST',
+        body: newPatientData
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        return Promise.reject(response);
+    }).then(() => {
+        $patientTable.row.add( [ 
+            `${data._id}`,
+            `${data.surname}`,
+            `${data.firstname}`,
+            `${moment(data.dob).format('DD/MM/YYYY')}`,
+            `${data.phonenumber}`] )
+        .draw();
+        formControls.forEach(el => el.value = '');
         newPatientForm.style.display = 'none';
-        document.querySelectorAll('.form-control').forEach(el => el.value = '');
-        newPatientButton.style.display = 'block';
+        newPatientButton.style.display = 'block';                    
     });
-
-    // // Post new patient
-    // $newPatientForm.submit(function(e){
-    //     e.preventDefault();
-    //     const $newPatientData = $(this).serialize();
-    //     $.post('/patients', $newPatientData, function(data){
-    //         $patientTable.row.add( [ 
-    //             `${data._id}`,
-    //             `${data.surname}`,
-    //             `${data.firstname}`,
-    //             `${moment(data.dob).format('DD/MM/YYYY')}`,
-    //             `${data.phonenumber}`] )
-    //         .draw();
-    //         $newPatientForm.find('.form-control').val('');
-    //         $newPatientButton.prop('disabled',false);
-    //         $newPatientForm.toggle();
-    //         $newPatientButton.toggle();    
-    //     });
-    // });
-
 });
