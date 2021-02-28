3658da938d93bed95e77720df8b6a4ed5d6a1d24
Andy McG <mcgow_a@hotmail.com>
2021-02-28 18:54:50 +0000

    still have to add fetch (put) functionality to edit patient

--- public/js/edit-patient.js
+++ public/js/edit-patient.js
@@ -1,38 +1,3 @@
-// $.fn.dataTable.moment( 'DD/MM/YYYY HH:mm' );
-
-// // Patient Index DataTable initialisation
-// const $patientLogTable = $('#patient-log-table').DataTable( {
-//     "columnDefs":  
-//         [{
-//             "targets": [ 0, 1 ],
-//             "visible": false
-//         }],
-//     "order": [[ 2, "desc" ]], // in production will be better to default sorting to ID ie: [0], as date in seed data not accurate
-//     "lengthMenu": [[5, 10, 20, -1], [5, 10, 20, "All"]]
-// } );
-
-// // Prevents flicker on loading
-// $('#patient-log-table').show();    
-
-// $('#patient-log-table tbody').on('click', 'tr', function () {
-//     // Block access to patient records for reception user type
-//     if (`${$userType}` !== "Reception") {
-//         const data = $patientLogTable.row( this ).data();
-
-//         switch (data[1]){
-//             case "M":
-//                 window.location.assign(`${$patientId}/medhists/${data[0]}?currentView=log`);
-//                 break;
-//             case "I":
-//                 window.location.assign(`${$patientId}/interviews/${data[0]}?currentView=log`);
-//                 break;
-//             case "C":
-//                 window.location.assign(`${$patientId}/clinicals/${data[0]}?currentView=log`);
-//                 break;    
-//         }
-//     }
-// });
-
 const options = {
     valueNames: ['id', 'typecode', 'date_created', 'type', 'info', 'signed_off'],
     page: 5,
@@ -41,93 +6,89 @@ const options = {
     }]
 };
 
-// const patientLogTableRows = document.querySelectorAll('#patient-log-table tbody tr');
+const patientLogTableRows = document.querySelectorAll('#patient-log-table tbody tr');
 
-// const patientLogTableList = new List('patient-log', options);
-// // sort by date created in descending order
-// patientLogTableList.sort('date_created', { order: 'desc' });
+const patientLogTableList = new List('patient-log', options);
+// sort by id in descending order (NB testing seed data dates may be out of sync)
+patientLogTableList.sort('id', { order: 'desc' });
 
-// patientLogTableRows.forEach(row => {
-//     row.addEventListener('click', () => {
-//         // Block access to patient records for reception user type
-//         if (`${$userType}` !== "Reception") {
-//             const data = $patientLogTable.row( this ).data();
+patientLogTableRows.forEach(row => {
+    row.addEventListener('click', () => {
+        // Block access to patient records for reception user type
+        if (`${userTypePass}` !== "Reception") {
+            const data = patientLogTable.row( this ).data();
 
-//             switch (data[1]){
-//                 case "M":
-//                     window.location.assign(`${$patientId}/medhists/${data[0]}?currentView=log`);
-//                     break;
-//                 case "I":
-//                     window.location.assign(`${$patientId}/interviews/${data[0]}?currentView=log`);
-//                     break;
-//                 case "C":
-//                     window.location.assign(`${$patientId}/clinicals/${data[0]}?currentView=log`);
-//                     break;    
-//             }       
-//         }
-//     });
-// });
+            switch (data[1]){
+                case "M":
+                    window.location.assign(`${patientId}/medhists/${data[0]}?currentView=log`);
+                    break;
+                case "I":
+                    window.location.assign(`${patientId}/interviews/${data[0]}?currentView=log`);
+                    break;
+                case "C":
+                    window.location.assign(`${patientId}/clinicals/${data[0]}?currentView=log`);
+                    break;    
+            }       
+        }
+    });
+});
 
 // SHOW/EDIT FUNCTIONALITY
-
-// const $editForm = document.querySelector('.edit-form');
-const $editButton = document.querySelector('.edit-button');
-const $editFormControls = document.querySelectorAll('.edit-form .form-control');
-const $submitEditButton = document.querySelector('.submit-edit-button');
-const $cancelEditButton = document.querySelector('.cancel-edit-button');
-const $deleteButton = document.querySelector('.delete-button');
-const $viewDocumentsButton = document.querySelector('#view-documents-button');
-const $changeViewButton = document.querySelector('.change-view-button');
-
-// *** need to debug and change to no jQuery ***
-const $editForm = $('.edit-form');
+const editForm = document.querySelector('.edit-form');
+const editButton = document.querySelector('.edit-button');
+const editFormControls = document.querySelectorAll('.edit-form .form-control');
+const submitEditButton = document.querySelector('.submit-edit-button');
+const cancelEditButton = document.querySelector('.cancel-edit-button');
+const deleteButton = document.querySelector('.delete-button');
+const viewDocumentsButton = document.querySelector('#view-documents-button');
+const changeViewButton = document.querySelector('.change-view-button');
 
 // Enable Edit form and buttons
-$editButton.addEventListener('click', function(){
+editButton.addEventListener('click', function(){
     this.style.display = 'none';
-    $viewDocumentsButton.style.display = 'none';
-    $submitEditButton.style.display = 'block';
-    $cancelEditButton.style.display = 'inline-block';
-    $deleteButton.style.display = 'inline-block';
-    $editFormControls.forEach(control => {control.disabled = false});
-    $changeViewButton.style.display = 'none';
+    viewDocumentsButton.style.display = 'none';
+    submitEditButton.style.display = 'block';
+    cancelEditButton.style.display = 'inline-block';
+    deleteButton.style.display = 'inline-block';
+    editFormControls.forEach(control => {control.disabled = false});
+    changeViewButton.style.display = 'none';
 });
 
 function disableEditForm(){
-    $editButton.style.display = 'inline-block';
-    $viewDocumentsButton.style.display = 'inline-block';
-    $submitEditButton.style.display = 'none';
-    $cancelEditButton.style.display = 'none';
-    $deleteButton.style.display = 'none';
-    $editFormControls.forEach(control => {control.disabled = true});
-    $changeViewButton.style.display = 'block';
+    editButton.style.display = 'inline-block';
+    viewDocumentsButton.style.display = 'inline-block';
+    submitEditButton.style.display = 'none';
+    cancelEditButton.style.display = 'none';
+    deleteButton.style.display = 'none';
+    editFormControls.forEach(control => {control.disabled = true});
+    changeViewButton.style.display = 'block';
 }
 
 // Disable Edit functionaity
-$cancelEditButton.addEventListener('click', function(){    
-    $editFormControls.forEach(control => {
+cancelEditButton.addEventListener('click', function(){    
+    editFormControls.forEach(control => {
         control.value = control.defaultValue;
     }); 
     disableEditForm();
 });
 
-// Submit Edit Details
-$editForm.submit(function(e){
-    e.preventDefault();
-    const $data = $(this).serialize();
-    const $formAction = $(this).attr('action');
-    $.ajax({
-        url: $formAction,
-        data: $data,
-        method: 'PUT',
-    });
+// // Submit Edit Details
+// $editForm.submit(function(e){
+//     e.preventDefault();
+//     const $data = $(this).serialize();
+//     const $formAction = $(this).attr('action');
+//     $.ajax({
+//         url: $formAction,
+//         data: $data,
+//         method: 'PUT',
+//     });
 
-    // Make default values the updated values
-    $editFormControls.each(function(){
-        $(this).prop('defaultValue', $(this).val());
-    }); 
-    disableEditForm();    
-});
+//     // Make default values the updated values
+//     $editFormControls.forEach(control => {
+//         control.defaultValue = control.value;
+//     }); 
+//     disableEditForm();    
+// });
 
 // Deleting patients prompt, different text to common delete
 document.querySelector('#delete-patient-form').addEventListener('submit', e => {
