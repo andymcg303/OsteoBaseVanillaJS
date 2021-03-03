6ea3f297f003d5be494750a61df5e91ddaa2e4ee
Andy McG <mcgow_a@hotmail.com>
2021-03-03 08:13:55 +0000

    finished pagination

--- public/js/patient.js
+++ public/js/patient.js
@@ -24,35 +24,6 @@ patientTableRows.forEach(row => {
     row.addEventListener('click', openPatientDetails);
 });
 
-// Observe when pagination list changes to re-style
-const observer = new MutationObserver(() => {
-    stylePagination()
-});
-const pageList = document.querySelector('.pagination');
-observer.observe(pageList, {subtree: true, attributes: true});
-
-// pagination list styling with bootstrap helper function
-const stylePagination = () => {
-    // add required bootstrap classes
-    const pageListItem = document.querySelectorAll('.pagination li');
-    pageListItem.forEach(el => {
-        el.classList.add('page-item')
-        el.querySelector('a').classList.add('page-link');
-    });
-    // prevent infinite loop
-    observer.disconnect();
-}
-
-// reconnect observation of pagination list changes on selecting new page
-pageList.addEventListener('click', () => {
-    observer.observe(pageList, {subtree: true, attributes: true});
-});
-
-// Style pagination after DOMLoad, sort and search
-window.addEventListener('DOMContentLoaded', () => stylePagination());
-patientTableList.on('searchComplete', () => stylePagination())
-patientTableList.on('sortComplete', () => stylePagination());
-
 // New Patient functionality
 const newPatientButton = document.querySelector('#new-patient-button');
 const newPatientForm = document.querySelector('#new-patient-form');
