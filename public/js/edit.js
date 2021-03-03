88ca0ff88d8ed5779da2be1bf1da057d2d7c403d
Andy McG <mcgow_a@hotmail.com>
2021-03-03 11:20:40 +0000

    interim commit

--- public/js/edit.js
+++ public/js/edit.js
@@ -163,110 +163,107 @@ const $submitEditButton = document.querySelector('.submit-edit-button');
 const $cancelEditButton = document.querySelector('.cancel-edit-button');
 const $deleteButton = document.querySelector('.delete-button');
 const $signoffButton = document.querySelector('.signoff-button');
-const $viewDocumentsButton = document.querySelector('#view-documents-button');
-const $changeViewButton = document.querySelector('.change-view-button');
 
 // Enable Edit form and buttons
 $editButton.addEventListener('click', function(){
     this.style.display = 'none';
-    $viewDocumentsButton.toggle();
-    $submitEditButton.toggle();
-    $signoffButton.toggle();
-    $cancelEditButton.css('display', 'inline-block');
-    $deleteButton.css('display', 'inline-block');
-    $editFormControls.prop('disabled',false);
-    $changeViewButton.toggle();
+    $submitEditButton.style.display = 'block';
+    // $signoffButton.toggle();
+    $cancelEditButton.style.display = 'inline-block';
+    $deleteButton.style.display = 'inline-block';
+    $editFormControls.disabled = false;
 });
 
-function disableEditForm(){
-    $editButton.toggle();
-    $viewDocumentsButton.toggle();
-    $signoffButton.toggle();
-    $submitEditButton.hide();
-    $cancelEditButton.hide();
-    $deleteButton.hide();
-    $editFormControls.prop('disabled',true);
-    $changeViewButton.toggle();
-}
+// function disableEditForm(){
+//     $editButton.toggle();
+//     $viewDocumentsButton.toggle();
+//     $signoffButton.toggle();
+//     $submitEditButton.hide();
+//     $cancelEditButton.hide();
+//     $deleteButton.hide();
+//     $editFormControls.prop('disabled',true);
+//     $changeViewButton.toggle();
+// }
 
-// Disable Edit functionaity
-$cancelEditButton.click(function(){
-    $editFormControls.each(function(){
-        $(this).val($(this).prop('defaultValue'));
-    }); 
-    disableEditForm();
-});
+// // Disable Edit functionaity
+// $cancelEditButton.click(function(){
+//     $editFormControls.each(function(){
+//         $(this).val($(this).prop('defaultValue'));
+//     }); 
+//     disableEditForm();
+// });
 
-// Submit Edit Details
-$editForm.submit(function(e){
-    e.preventDefault();
-    const $data = $(this).serialize();
-    const $formAction = $(this).attr('action');
-    $.ajax({
-        url: $formAction,
-        data: $data,
-        method: 'PUT',
-    });
+// // Submit Edit Details
+// $editForm.submit(function(e){
+//     e.preventDefault();
+//     const $data = $(this).serialize();
+//     const $formAction = $(this).attr('action');
+//     $.ajax({
+//         url: $formAction,
+//         data: $data,
+//         method: 'PUT',
+//     });
 
-    // Make default values the updated values
-    $editFormControls.each(function(){
-        $(this).prop('defaultValue', $(this).val());
-    }); 
-    disableEditForm();    
-});
+//     // Make default values the updated values
+//     $editFormControls.each(function(){
+//         $(this).prop('defaultValue', $(this).val());
+//     }); 
+//     disableEditForm();    
+// });
 
-// Delete Prompt 
-$('.delete-form').submit(function(e){
-    const confirmResponse = confirm('Are you sure?');
-    if (!confirmResponse) {
-        e.preventDefault();
-    }
-});
+// // Delete Prompt 
+// $('.delete-form').submit(function(e){
+//     const confirmResponse = confirm('Are you sure?');
+//     if (!confirmResponse) {
+//         e.preventDefault();
+//     }
+// });
 
-// Deleting patients prompt, different text
-    $('#delete-patient-form').submit(function(e){
-    const confirmResponse = confirm('This will delete all the patients details including clinical details which by law you are required to keep for 6 years. Are you sure?');
-    if (!confirmResponse) {
-        e.preventDefault();
-    }
-});
+// // Deleting patients prompt, different text
+//     $('#delete-patient-form').submit(function(e){
+//     const confirmResponse = confirm('This will delete all the patients details including clinical details which by law you are required to keep for 6 years. Are you sure?');
+//     if (!confirmResponse) {
+//         e.preventDefault();
+//     }
+// });
 
 // SIGNOFF FUNCTIONALITY
 // Show Edit button and Signoff button if not signed off
-if ($('.edit-form .hidden-signed-off').val() === "false"){
-    $('.signoff-edit-button').css('display', 'inline-block');
-    $signoffButton.css('display', 'inline-block');
+const signedOff = document.querySelector('.edit-form .hidden-signed-off').value;
+if (signedOff === "false") {
+    document.querySelector('.signoff-edit-button').style.display = 'inline-block';
+    $signoffButton.style.display = 'inline-block';
 }
 
-// Perform signoff 
-$signoffButton.click(function(){
-    const confirmResponse = confirm('This will permanently lock this item from further editing. Are you sure?');
-    if (confirmResponse) {
-        $('.hidden-signed-off').val(true);
-        // trigger edit form submission ie update with signed off value
-        $editForm.trigger('submit');
-    }
-});
+// // Perform signoff 
+// $signoffButton.click(function(){
+//     const confirmResponse = confirm('This will permanently lock this item from further editing. Are you sure?');
+//     if (confirmResponse) {
+//         $('.hidden-signed-off').val(true);
+//         // trigger edit form submission ie update with signed off value
+//         $editForm.trigger('submit');
+//     }
+// });
 
-//TOGGLE HISTORY DISPLAY FUNCTIONALITY
-const $historyButton = $('.history-button');
+// //TOGGLE HISTORY DISPLAY FUNCTIONALITY
+// const $historyButton = $('.history-button');
 
-$historyButton.click(function(){
-    $('.main-container').toggleClass('container').toggleClass('container-fluid');
-    $('.history-scroll').toggle();
-    $('.inputs-col').toggleClass('col-12').toggleClass('col-lg-8');
-    $(this).text(function(i, text){
-        return text === "Show History" ? "Hide History" : "Show History";
-    });
-})
+// $historyButton.click(function(){
+//     $('.main-container').toggleClass('container').toggleClass('container-fluid');
+//     $('.history-scroll').toggle();
+//     $('.inputs-col').toggleClass('col-12').toggleClass('col-lg-8');
+//     $(this).text(function(i, text){
+//         return text === "Show History" ? "Hide History" : "Show History";
+//     });
+// })
 
-//Reformat main container if window width reduced by user and history view selected
-$(window).resize(function() {
-    if ($(window).width() < 992 && $historyButton.text() === "Hide History") { // smaller than BS xl? window size
-        $('.main-container').removeClass('container-fluid');
-        $('.main-container').addClass('container');
-    } else if ($(window).width() >= 992 && $historyButton.text() === "Hide History") { //greater or equal to BS xl? window size 
-        $('.main-container').removeClass('container');
-        $('.main-container').addClass('container-fluid');
-    }
-});
+// //Reformat main container if window width reduced by user and history view selected
+// $(window).resize(function() {
+//     if ($(window).width() < 992 && $historyButton.text() === "Hide History") { // smaller than BS xl? window size
+//         $('.main-container').removeClass('container-fluid');
+//         $('.main-container').addClass('container');
+//     } else if ($(window).width() >= 992 && $historyButton.text() === "Hide History") { //greater or equal to BS xl? window size 
+//         $('.main-container').removeClass('container');
+//         $('.main-container').addClass('container-fluid');
+//     }
+// });
