82cacba53f49aeb7b67f8ec593d216f75ddc495b
Andy McG <mcgow_a@hotmail.com>
2021-04-05 18:15:10 +0100

    Next: if history scroll open for new item, keep open for edit view

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,16 +1,16 @@
-// list.js configuration for history scroll
-const options = {
-    valueNames: [
-        'type',
-        {name: 'date-created', attr: 'timestamp'},
-        'data'
-    ]
-};
+// // list.js configuration for history scroll
+// const options = {
+//     valueNames: [
+//         'type',
+//         {name: 'date-created', attr: 'timestamp'},
+//         'data'
+//     ]
+// };
   
-let historyList = new List('history-scroll-id', options);
+// let historyList = new List('history-scroll-id', options);
 
-// sort form newest patient in descending order
-historyList.sort('date-created', { order: 'desc'});
+// // sort form newest patient in descending order
+// historyList.sort('date-created', { order: 'desc'});
 
 // SHOW/EDIT FUNCTIONALITY
 const editForm = document.querySelector('.edit-form');
@@ -248,40 +248,5 @@ signoffButton.addEventListener('click', () => {
         document.querySelector('.hidden-signed-off').value = "true";
         // trigger edit form submission ie update with signed off value
         editForm.requestSubmit();
-    }
-});
-
-//TOGGLE HISTORY DISPLAY FUNCTIONALITY
-const historyButton = document.querySelector('.history-button');
-const mainContainer = document.querySelector('.main-container');
-
-historyButton.addEventListener('click', function(){ 
-    mainContainer.classList.toggle('container');
-    mainContainer.classList.toggle('container-fluid');
-
-    const historyScroll = document.querySelectorAll('.history-scroll');
-    if (window.getComputedStyle(historyScroll[0]).display === 'none') {
-        historyScroll.forEach(el => el.style.display = 'block');
-    } else {
-        historyScroll.forEach(el => el.style.display = 'none');
-    }
-
-    const inputsCol = document.querySelector('.inputs-col');
-    inputsCol.classList.toggle('col-12');
-    inputsCol.classList.toggle('col-lg-8');
-
-    this.textContent = this.textContent === "Show History" ? "Hide History" : "Show History";
-    
-});
-
-//Reformat main container if window width reduced by user and history view selected
-window.addEventListener('resize', () => {
-    const mainContainer = document.querySelector('.main-container');
-    if (window.innerWidth < 992 && historyButton.textContent === "Hide History") { // smaller than BS xl? window size
-        mainContainer.classList.remove('container-fluid');
-        mainContainer.classList.add('container');
-    } else if (window.innerWidth >= 992 && historyButton.textContent === "Hide History") { //greater or equal to BS xl? window size
-        mainContainer.classList.remove('container');
-        mainContainer.classList.add('container-fluid');
     }
 });
---
+++ public/js/history.js
@@ -0,0 +1,48 @@
+// list.js configuration for history scroll
+const options = {
+    valueNames: [
+        'type',
+        {name: 'date-created', attr: 'timestamp'},
+        'data'
+    ]
+};
+  
+let historyList = new List('history-scroll-id', options);
+
+// sort form newest patient in descending order
+historyList.sort('date-created', { order: 'desc'});
+
+//TOGGLE HISTORY DISPLAY FUNCTIONALITY
+const historyButton = document.querySelector('.history-button');
+const mainContainer = document.querySelector('.main-container');
+
+historyButton.addEventListener('click', function(){ 
+    mainContainer.classList.toggle('container');
+    mainContainer.classList.toggle('container-fluid');
+
+    const historyScroll = document.querySelectorAll('.history-scroll');
+    if (window.getComputedStyle(historyScroll[0]).display === 'none') {
+        historyScroll.forEach(el => el.style.display = 'block');
+    } else {
+        historyScroll.forEach(el => el.style.display = 'none');
+    }
+
+    const inputsCol = document.querySelector('.inputs-col');
+    inputsCol.classList.toggle('col-12');
+    inputsCol.classList.toggle('col-lg-8');
+
+    this.textContent = this.textContent === "Show History" ? "Hide History" : "Show History";
+    
+});
+
+//Reformat main container if window width reduced by user and history view selected
+window.addEventListener('resize', () => {
+    const mainContainer = document.querySelector('.main-container');
+    if (window.innerWidth < 992 && historyButton.textContent === "Hide History") { // smaller than BS xl? window size
+        mainContainer.classList.remove('container-fluid');
+        mainContainer.classList.add('container');
+    } else if (window.innerWidth >= 992 && historyButton.textContent === "Hide History") { //greater or equal to BS xl? window size
+        mainContainer.classList.remove('container');
+        mainContainer.classList.add('container-fluid');
+    }
+});
