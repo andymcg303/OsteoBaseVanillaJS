c0ee56893e99254c97d9aefcadfd7424f75dc736
Andy McG <mcgow_a@hotmail.com>
2021-04-25 12:39:22 +0100

    showHistory working for show medhist. Next: applly to rest of show item forms and new forms

--- public/js/history.js
+++ public/js/history.js
@@ -1,34 +1,41 @@
-// list.js configuration for history scroll
-const options = {
-    valueNames: [
-        'type',
-        {name: 'date-created', attr: 'timestamp'},
-        'data'
-    ]
+const urlParams = new URLSearchParams(window.location.search);
+const showHistory = urlParams.get('showHistory');
+
+if (showHistory === 'true') {
+
+    // list.js configuration for history scroll
+    const options = {
+        valueNames: [
+            'type',
+            {name: 'date-created', attr: 'timestamp'},
+            'data'
+        ]
+    };
+
+    let historyList = new List('history-scroll-id', options);
+
+    // sort form newest patient in descending order
+    historyList.sort('date-created', { order: 'desc'});
+
+    //History Display Functionality ie formatting with bootstrap
+    const mainContainer = document.querySelector('.main-container');
+        
+    mainContainer.classList.toggle('container');
+    mainContainer.classList.toggle('container-fluid');
+
+    const inputsCol = document.querySelector('.inputs-col');
+    inputsCol.classList.toggle('col-12');
+    inputsCol.classList.toggle('col-lg-8');
+
+    //Reformat main container if window width reduced by user and history view selected
+    window.addEventListener('resize', () => {
+        if (window.innerWidth < 992) { // smaller than BS xl? window size
+            mainContainer.classList.remove('container-fluid');
+            mainContainer.classList.add('container');
+        } else if (window.innerWidth >= 992) { //greater or equal to BS xl? window size
+            mainContainer.classList.remove('container');
+            mainContainer.classList.add('container-fluid');
+        }
+    });
+
 };
-  
-let historyList = new List('history-scroll-id', options);
-
-// sort form newest patient in descending order
-historyList.sort('date-created', { order: 'desc'});
-
-//History Display Functionality ie formatting with bootstrap
-const mainContainer = document.querySelector('.main-container');
-      
-mainContainer.classList.toggle('container');
-mainContainer.classList.toggle('container-fluid');
-
-const inputsCol = document.querySelector('.inputs-col');
-inputsCol.classList.toggle('col-12');
-inputsCol.classList.toggle('col-lg-8');
-
-//Reformat main container if window width reduced by user and history view selected
-window.addEventListener('resize', () => {
-    if (window.innerWidth < 992) { // smaller than BS xl? window size
-        mainContainer.classList.remove('container-fluid');
-        mainContainer.classList.add('container');
-    } else if (window.innerWidth >= 992) { //greater or equal to BS xl? window size
-        mainContainer.classList.remove('container');
-        mainContainer.classList.add('container-fluid');
-    }
-});
